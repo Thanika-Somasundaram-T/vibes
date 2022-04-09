dca98fb1d98110b530d00dc28367a6ec7185f0ba
Thanika_Somasundaram_T <thanikasomasundaram1801@gmail.com>
2022-04-09 10:52:15 +0530

    Initialized Login.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import './App.css';
+import Login from './components/login';
 
 function App() {
 	return (
 		<div className="App">
-		VIBES
+		<h1>Vibes</h1>
+		<Login/>
 		</div>
 	);
 }
