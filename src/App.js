461f51a5462bdff5ddb35b932bb4a2f0922f0a90
Thanika_Somasundaram_T <thanikasomasundaram1801@gmail.com>
2022-04-26 13:14:39 +0530

    Made loginUI.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Login from './components/login';
 function App() {
 	return (
 		<div className="App">
-		<h1>Vibes</h1>
 		<Login/>
 		</div>
 	);
--- src/components/login.js
+++ src/components/login.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Login = () => {
 	return(
-		<h1>Login</h1>
+		<div className="login">
+			<img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="spotify"></img>
+			<a href="">LOGIN WITH SPOTIFY</a>
+		</div>
+
 	);
 }
 
